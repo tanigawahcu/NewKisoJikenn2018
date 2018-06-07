f6c254a05317dd1f3f92b0815893847193d1eef3
tanigawahcu <tanigawa@hiroshima-cu.ac.jp>
2018-06-07 11:19:30 +0900

    03StackTest is passed

--- 07/python/01/CodeWriter.py
+++ 07/python/01/CodeWriter.py
@@ -1,8 +1,13 @@
 import Parser
+import os
 
 class CodeWriter():
     def __init__(self, fname):
         self.fout = open(fname, 'w')
+        self.label_num = 0
+        label = os.path.basename(fname)     # ディレクトリ名を削除
+        label,ext = os.path.splitext(label) # 拡張子を分離
+        self.label_index = label+'.'
 
     def writeArithmetic(self, command):
         if command.find('add') >= 0 :
@@ -20,12 +25,13 @@ class CodeWriter():
             self.writeVMinst(list)
 
         elif command.find('eq') >= 0 :
-            self.writeMicroPop()
-            self.fout.write('@SP\n')
-            self.fout.write('A=M-1\n')
-            self.fout.write('D=M-D\n')
-            self.fout.write('@LOOP'+loop_num)
-            D;
+            self.writeCompInst('JEQ')
+
+        elif command.find('gt') >= 0 :
+            self.writeCompInst('JGT')
+
+        elif command.find('lt') >= 0 :
+            self.writeCompInst('JLT')
 
         elif command.find('and') >= 0 :
             self.writeMicroPop()
@@ -66,3 +72,17 @@ class CodeWriter():
     def writeVMinst(self, vmlist) :
         for vm in vmlist:
             self.fout.write(vm+'\n')
+
+    # 比較命令の実装（分岐命令の種類の違いしかないので共通化
+    def writeCompInst(self, branch) :
+        self.writeMicroPop()
+        true_label = str(self.label_index)+'IFTRUE'+str(self.label_num)
+        end_label  = str(self.label_index)+'IFEND'+str(self.label_num)
+
+        list = ['@SP', 'A=M-1', 'D=M-D','@'+true_label,
+                'D;'+branch, '@SP', 'A=M-1', 'M=0', '@'+end_label,
+                '0;JMP', '('+true_label+')', '@SP', 'A=M-1', 'M=-1',
+                '('+end_label+')']
+        self.writeVMinst(list)
+
+        self.label_num = self.label_num + 1
